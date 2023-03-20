37b633e114c920fd18b07da2a8bf4f9adafd909d
iddrismohammed <iddrismohammed73@gmail.com>
2023-03-19 19:35:09 -0700

    HelloPrint

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
-*main - entry point
+*main - Entry point
 *Return: always 0
 */
-
 int main(void)
 {
-printf("With proper grammer, but outcome is apeace of art,\n");
+printf("with proper grammr, but the outcome is a piece of art,\n");
 return (0);
 }
